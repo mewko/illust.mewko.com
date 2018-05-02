a45c47b3d14632da38b8ae7a6c474e7be7196a3e
xsota <git@xsota.com>
2018-05-02 20:17:50 +0900

    vuetify default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,52 +1,98 @@
 <template>
-  <div>
-    <nuxt/>
+  <div id="app">
+    <v-app>
+      <v-navigation-drawer
+        fixed
+        app
+        v-model="drawer"
+      >
+        <v-list dense class="pt-0">
+          <v-list-tile to="/">
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>トップページ</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile to="/category/hoge">
+            <v-list-tile-action>
+              <v-icon>whatshot</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>カテゴリとかそういうの</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile to="/category/hoge">
+            <v-list-tile-action>
+              <v-icon>star</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>カテゴリとかそういうの</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-toolbar color="primary" dark fixed app>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
+        <v-toolbar-title>ここにタイトル？</v-toolbar-title>
+        <v-spacer/>
+
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat to="/search">
+            なんか検索ボタンとか？
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+
+      <v-content>
+        <transition name="fade" mode="out-in" appear>
+          <nuxt/>
+        </transition>
+      </v-content>
+
+      <v-footer color="primary" dark>
+        <v-flex xs12 py-3 text-xs-center white--text>
+          &copy; 2018 <strong>mewko</strong>
+        </v-flex>
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        drawer: false
+      }
+    }
+  }
+</script>
 
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
+<style lang="scss">
+  .fade-enter-active {
+    transition: all 0.2s;
+  }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+  .fade-leave-active {
+    transition: all 0.3s;
+  }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
+  .fade-enter {
+    // transform: translateX(20px);
+    opacity: 0;
+  }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+  .fade-leave-to {
+    opacity: 0;
+  }
 </style>
